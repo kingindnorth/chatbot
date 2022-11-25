8ea740995e274062d75b59239382513c9c652e54
prajjawal tiwari <kingindnorth.0311@gmail.com>
2022-11-25 12:08:40 +0530

    static files

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require("express")
 const http = require("http")
 const socketio =require("socket.io")
+const { PassThrough } = require("stream")
 require("dotenv").config()
 
 const app = express()
 const server = http.createServer(app)
+const io = socketio(server)
+
+app.use(express.static("public"))
+
+io.on("connection",socket=>{
+    Pass
+})
+
 
 
 PORT = process.env.PORT || 8000
